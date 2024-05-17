922bbb9010ad9073179a5473f268d59a0a7afba9
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-05-17 10:46:04 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="22cun"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/main/kotlin/com/midas/pharmacy/PharmacyApplication.kt
@@ -0,0 +1,11 @@
+package com.midas.pharmacy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PharmacyApplication
+
+fun main(args: Array<String>) {
+    runApplication<PharmacyApplication>(*args)
+}
