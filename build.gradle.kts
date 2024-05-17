22576067fed68a74b214066751a827eb398ae545
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-05-17 16:40:54 +0900

    [feat] gradle 설정
    gradle 수정 app.jar로 생성한다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,3 +47,7 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.bootJar {
+    archiveFileName = "app.jar"
+}
--- src/main/kotlin/com/midas/pharmacy/PharmacyApplication.kt
+++ src/main/kotlin/com/midas/pharmacy/PharmacyApplication.kt
@@ -1,4 +1,4 @@
-package com.midas.pharmacy
+ package com.midas.pharmacy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
