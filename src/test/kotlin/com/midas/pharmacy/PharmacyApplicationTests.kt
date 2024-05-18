4365d826f962bdcc9d408a7da7edcbf9e48233d4
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-05-18 14:15:23 +0900

    [test] init 테스트 코드 수정
    h2를 사용, value 값 입력

--- src/test/kotlin/com/midas/pharmacy/PharmacyApplicationTests.kt
+++ src/test/kotlin/com/midas/pharmacy/PharmacyApplicationTests.kt
@@ -2,8 +2,10 @@ package com.midas.pharmacy
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
+@SpringBootTest(properties = ["kakao.rest.api.key=your-test-api-key"])
+@ActiveProfiles("testdb")
 class PharmacyApplicationTests {
 
     @Test
